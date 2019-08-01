9f3570a8c1d04d38fa000d4b21e5a32c56bdd2a1
Vladimir Smirnov <vladimir.igo@gmail.com>
2019-08-01 19:02:04 +0300

    Create registerWarningSuppresion.cs

---
+++ revit-macro/registerWarningSuppresion.cs
@@ -0,0 +1,19 @@
+public void registerWarningSuppresion()
+// https://boostyourbim.wordpress.com/2018/08/08/biltna2018-wish-granted-remove-warning-when-modifying-type-parameter-values-in-schedule/
+{
+    Document doc = this.ActiveUIDocument.Document;   
+    Application app = doc.Application;
+    UIApplication uiapp = new UIApplication(app);
+    uiapp.DialogBoxShowing += new EventHandler(dismissTaskDialog);
+}
+
+private void dismissTaskDialog(object sender, DialogBoxShowingEventArgs args)
+{
+    TaskDialogShowingEventArgs e = args as TaskDialogShowingEventArgs;
+    if (e == null)
+        return;
+    if (e.Message.StartsWith("This change will be applied to all elements of type"))
+    {
+        e.OverrideResult((int)TaskDialogCommonButtons.Ok);
+    }
+}
